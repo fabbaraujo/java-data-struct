cbe9e76aabbfed4900fc9113fd7b2198bd744fb9
Fabrício Araújo <fabaraujo23@gmail.com>
2020-11-08 21:12:02 -0300

    Básico de priority queue

---
+++ priorityqueue/priorityqueue1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class PriorityQueues {
+    public static void main(String[] args) {
+
+        PriorityQueue<String> q = new PriorityQueue<String>();
+
+        q.offer("first");
+        q.offer("second");
+        q.offer("third");
+
+        System.out.printf("%s ", q);
+        System.out.println();
+
+        System.out.printf("%s ", q.peek());
+        System.out.println();
+
+        q.poll();
+        System.out.printf("%s ", q);
+    }
+}
